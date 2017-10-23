140bd2a24bf8c0d7bd3db98c13aae994d14ca295
RohanParikh <RohanParikh@192.168.0.101>
2017-10-23 18:13:44 -0400

    Lesson02 and ProjectCarSelector

---
+++ src/Lesson02.java
@@ -0,0 +1,39 @@
+//Java Coding Circle Lesson 2
+
+import java.util.Scanner;
+
+public class Lesson02 {
+	public static void main(String args[]) {
+		//See if 1 is greater than or equal to 8
+		
+		//TRY IT: See if 5.0000001 is greater than 5
+
+		
+		
+		
+		//TRY IT: Set did_Homework = false. 
+		//Check if it is true, then print out “You can play a video game”
+		//If it is false, print out “Go get your backpack.”
+		
+		
+		
+		//Determine output of: (not(true and false) or (not(true) or not(false))) and (true or true)
+		
+		//Set cleaned_room equal to true and took_out_trash equal to false
+		
+		//TRY IT: Check if the room is clean or the trash is taken out and if so print “Here is your allowance”
+
+		
+		
+		//USE NESTED CONDITIONALS FOR THE FOLLOWING ACTIVITY
+		//Take a user's input that asks "How many candies do you want?"
+		//If the user input is less than 2, print "That's it?"
+		//Otherwise, if the user's input is less than 5 but greater than 2, print "That's a good amount"
+		//If the user's input is greater than 5, print "Too much candy!"
+		
+		
+		//TRY IT: Try taking the square root of a negative number. If there is an error, print “What did you think would happen?”
+
+		
+	}
+}
---
+++ src/ProjectCarSelector.java
@@ -0,0 +1,8 @@
+public class ProjectCarSelector
+{
+	public static void main(String[] args)
+	{
+		//Look at and follow the directions listed under project at the bottom of your notes.
+		
+	}
+}
